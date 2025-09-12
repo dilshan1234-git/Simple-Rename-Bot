736707e2840f083154241d55bdb965f260f27624
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2025-09-12 12:36:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "14631157"))
 API_HASH = environ.get("API_HASH", "aa7c2b3be68a7488abdb9de6ce78d311")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7662948776:AAEHGmPLApWXjT1OIFavQhST90Tkm-32CCc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7662948776:AAHdgUgb2IjeFsWZXCmK4XxYmKdOKi_kLts")
 TG_MAX_FILE_SIZE = 2097152000  # 2GB for Telegram
 CHUNK_SIZE = 1024 * 1024  # 1MB
 PROCESS_MAX_TIMEOUT = 300  # 5 minutes
@@ -21,4 +21,4 @@ START_IMAGE_URL = "https://envs.sh/klj.jpg"
 MEGA_EMAIL = "dinethinfinity123@gmail.com"
 MEGA_PASSWORD = "mega1234"
 
-INSTA_SESSIONID = "75426134830%3AcHrfy1okPf6vwR%3A13%3AAYeeMXMU-grqJf3MYW-CAyxbdxOazPoDSX0kAh8Z5w"
+
