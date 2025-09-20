10cca5c7ab30b6b357af1172eeccd6758c6cd67c
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2025-09-20 12:57:41 +0530

    Update progress_hook.py

--- main/downloader/progress_hook.py
+++ main/downloader/progress_hook.py
@@ -72,7 +72,7 @@ class YTDLProgress:
                         total_bytes = data.get('total_bytes', 0)
                         text = (
                             f"✅ **Download Completed!**\n\n"
-                            f"**📂 File:** {filename}\n"
+                            f"**📂 File:** {filename}\n\n"
                             f"**💾 Total Size:** {humanbytes(int(total_bytes)) if total_bytes > 0 else 'Unknown'}"
                         )
                         await self._update_msg(text)
