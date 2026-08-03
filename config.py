7a67f0932918a9a0fcc212810cc014ff2fcc28e7
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2026-08-03 10:53:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "14631157"))
 API_HASH = environ.get("API_HASH", "aa7c2b3be68a7488abdb9de6ce78d311")
 BOT_TOKEN = environ.get(
     "BOT_TOKEN",
-    "7662948776:AAFjrEWZ2qkEVFSCd9-Iez9arAiEc6HTDGk")
+    "7662948776:AAGu_vIuk89zmRdCc_96pH8wJR_bxcD_Zck")
 TG_MAX_FILE_SIZE = 2097152000  # 2GB for Telegram
 CHUNK_SIZE = 1024 * 1024  # 1MB
 PROCESS_MAX_TIMEOUT = 300  # 5 minutes
